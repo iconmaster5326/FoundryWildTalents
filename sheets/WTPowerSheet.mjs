b2e958d7a1dd60b134dc9b90fd74a5b0846ce122
iconmaster5326 <crazybonesj@yahoo.com>
2023-11-11 16:47:16 -0500

    Power sheets, part 2

--- sheets/WTPowerSheet.mjs
+++ sheets/WTPowerSheet.mjs
@@ -62,6 +62,7 @@ export class WTPowerSheet extends WTItemSheet {
         "system.qualities": newArray,
       });
     });
+
     html.find(".skillslot").dblclick(async (event) => {
       event.preventDefault();
       Item.get(this.item.system.skill.id).sheet.render(true);
@@ -80,6 +81,58 @@ export class WTPowerSheet extends WTItemSheet {
         },
       },
     ]);
+
+    html.find(".add-extra").click((event) => {
+      event.preventDefault();
+      const newArray = this.item.system.qualities.slice();
+      const i = Number(event.currentTarget.getAttribute("index"));
+      newArray[i].extras = newArray[i].extras.concat([{}]);
+      this.item.update({
+        "system.qualities": newArray,
+      });
+    });
+    html.find(".remove-extra").click((event) => {
+      event.preventDefault();
+      const i = Number(event.currentTarget.getAttribute("index1"));
+      const j = Number(event.currentTarget.getAttribute("index2"));
+      const newArray = this.item.system.qualities.slice();
+      newArray[i].extras = newArray[i].extras
+        .slice(0, j)
+        .concat(newArray[i].extras.slice(j + 1));
+      this.item.update({
+        "system.qualities": newArray,
+      });
+    });
+
+    html.find(".extraslot").dblclick(async (event) => {
+      event.preventDefault();
+      const i = Number(event.currentTarget.getAttribute("index1"));
+      const j = Number(event.currentTarget.getAttribute("index2"));
+      Item.get(this.item.system.qualities[i].extras[j].id).sheet.render(true);
+    });
+    ContextMenu.create(this, html, ".extraslot", [
+      {
+        name: game.i18n.localize("WT.Dialog.RemoveExtra"),
+        icon: "",
+        condition: (slot) => {
+          const i = Number(slot.attr("index1"));
+          const j = Number(slot.attr("index2"));
+          return this.item.system.qualities[i].extras[j].id;
+        },
+        callback: async (slot) => {
+          const i = Number(slot.attr("index1"));
+          const j = Number(slot.attr("index2"));
+          const newArray = this.item.system.qualities.slice();
+          newArray[i].extras[j] = {
+            ...newArray[i].extras[j],
+            id: undefined,
+          };
+          this.item.update({
+            "system.qualities": newArray,
+          });
+        },
+      },
+    ]);
   }
 
   /** @override */
@@ -91,6 +144,32 @@ export class WTPowerSheet extends WTItemSheet {
     if (item.type == "skill" && this.item.system.powerType == 1) {
       this.item.update({ "system.skill.id": itemID });
     } else if (item.type == "extra") {
+      if (item.system.fociOnly) return;
+      const slots = document
+        .elementsFromPoint(event.pageX, event.pageY)
+        .filter((e) => e.classList.contains("extraslot"));
+      const qualities = document
+        .elementsFromPoint(event.pageX, event.pageY)
+        .filter((e) => e.classList.contains("quality"));
+      if (slots.length) {
+        const i = Number(slots[0].getAttribute("index1"));
+        const j = Number(slots[0].getAttribute("index2"));
+        const newArray = this.item.system.qualities.slice();
+        newArray[i].extras[j] = {
+          ...newArray[i].extras[j],
+          id: item.id,
+        };
+        this.item.update({
+          "system.qualities": newArray,
+        });
+      } else if (qualities.length) {
+        const i = Number(qualities[0].getAttribute("index"));
+        const newArray = this.item.system.qualities.slice();
+        newArray[i].extras = newArray[i].extras.concat([{ id: itemID }]);
+        this.item.update({
+          "system.qualities": newArray,
+        });
+      }
     }
   }
 }
